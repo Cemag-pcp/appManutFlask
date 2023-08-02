435f3385b01f899bb61ce898660f71eb86b3eba9
TI <pcp@cemag.com.br>
2023-08-02 15:04:11 -0300

    dashss

--- static/js/paginacao.js
+++ static/js/paginacao.js
@@ -44,6 +44,10 @@ $(document).ready(function() {
         "aLengthMenu": [[3, 5, 10, 25, -1], [3, 5, 10, 25, "All"]],
         "iDisplayLength": 6
     });
+    $('#tabela_graficos9').DataTable({
+        "aLengthMenu": [[3, 5, 10, 25, -1], [3, 5, 10, 25, "All"]],
+        "iDisplayLength": 6
+    });
     // Oculta o campo "Show entries"
     $('div.dataTables_length').hide();
 });
