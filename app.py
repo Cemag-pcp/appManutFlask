4d9b137bd0d1fd48caeddd94f286c21c82d57a08
luan <luan640@gmail.com>
2024-02-29 09:20:40 -0300

    retornando versao normal

--- app.py
+++ app.py
@@ -5,14 +5,11 @@ import psycopg2 #pip install psycopg2
 import psycopg2.extras
 import os
 
-def create_app():
-    app = Flask(__name__)
-    app.config['UPLOAD_FOLDER'] = r'C:\Users\pcp2\projetoManutencao\appManutFlask-3\UPLOAD_FOLDER'
-    app.register_blueprint(routes_bp)
-    app.register_blueprint(login_bp)
-    app.secret_key = "manutencaoprojeto"
-
-    return app
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = r'C:\Users\pcp2\projetoManutencao\appManutFlask-3\UPLOAD_FOLDER'
+app.register_blueprint(routes_bp)
+app.register_blueprint(login_bp)
+app.secret_key = "manutencaoprojeto"
 
 # DB_HOST = "localhost"
 DB_HOST = "database-1.cdcogkfzajf0.us-east-1.rds.amazonaws.com"
@@ -26,5 +23,5 @@ conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_
 # def index():
 #     return 'Página Inicial'
 
-# if __name__ == '__main__':
-#     app.run()
+if __name__ == '__main__':
+    app.run()
